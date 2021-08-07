0181b5778f158ea22b2b7c3ce1abb7904b776ec7
chakane <chakanezshegog@gmail.com>
2021-08-07 17:06:19 -0400

    divide and conquer

--- index.js
+++ index.js
@@ -15,7 +15,51 @@ const play = () => {
   }
 }
 
-const startGame = () => {}
+const startGame = () => {
+    let guessCount = 25
+    let bottom = 1
+    let top = 10000
+    while(guessCount > 0){
+        const guess = Math.floor((bottom+top)/2)  // the main divide and conquer part
+        console.log("My guess is  " + guess)
+
+        if(readlineSync.keyInYN("Am I right?")) {
+            gameOver(true)
+        } else {
+          guessCount--
+          console.log("Yikes! I only have " + guessCount + " guesses left!")
+          let highOrLow
+
+          while(highOrLow !== "high" && highOrLow !== "low") {
+            highOrLow = readlineSync.question("Was I too high or too low? \n")
+            highOrLow = highOrLow.toLowerCase().trim()
+          }
+          console.log("thanks, ill guess better from now on")
+          if(highOrLow === "high") {
+            top = guess - 1
+          } else {
+            bottom = guess + 1
+          }
+        }
+      }
+      gameOver(false)
+  }
+
+const gameOver = (computerWin) => {
+  if(computerWin) {
+    console.log("I win")
+    console.log("I WIN!!!!")
+    console.log("COMPUTERS ARE OKAY I GUESS")
+  } else {
+    console.log("Congrats, i'll win next time")
+  }
+  if(readlineSync.keyInYN("PLAY AGAIN?")) {
+    play()
+  } else {
+    leaveGame()
+  }
+}
+  
 
 const leaveGame = () => {
   console.log("That's too bad :-( Good luck in life!")
